306f643cd77a6a76031d429ec8492029ff5467f3
Xuheng Liu <liuxuheng1990@gmail.com>
2022-07-22 22:51:17 +0800

    feat: Bug 2: no matter the route change, histroy.location.pathname won't change in onRouteChange

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
+import { history } from 'umi';
 import { consoleHistory as consoleHistoryUnderUtilsFolder } from '@/utils/consoleHistory';
 
 // Bug 1: the history object consoled here is undefined
 consoleHistoryUnderUtilsFolder();
+
+export const onRouteChange = () => {
+  // Bug 2: no matter the route change, histroy.location.pathname won't change
+  console.log(history.location.pathname);
+};
